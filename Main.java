a9354800977d25b78ded89c478cb022375ebf95d
atarela <36445782+atarela@users.noreply.github.com>
2019-06-06 21:00:37 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -16,6 +16,13 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+         PageAccueil accueil =new PageAccueil();
+         
+       accueil.setVisible(true);
+       
     }
+
+
     
 }
